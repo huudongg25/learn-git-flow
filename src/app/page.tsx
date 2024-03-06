48796a9a8e520dcdff204d595dfdd0ca53f15d79
milotriss <nhattienla98@gmail.com>
2024-03-06 14:11:25 +0700

    about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Banner from "@/components/banners/banner";
 import styles from "./page.module.css";
+import About from "./about/page";
 
 export default function Index() {
-  return <main className={styles.main}></main>;
+  return <main className={styles.main}>
+    <Banner/>
+    <About/>
+  </main>;
 }
